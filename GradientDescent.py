dc339e758ce1028dfd30ad4af33d207b31bfd716
Neo2854 <pavansantoshls990@gmail.com>
2022-08-31 14:51:09 +0530

    Gradient Descent Completed

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,1 +1,26 @@
-import numpy
+import numpy as np
+
+from config import *
+
+def GradientDescent(deriv, x0, lr = 0.01, tol = 0.00001, max_iters = 10000, verbose = 0):
+    curr_tol = tol * 1000
+    dir = np.zeros(x0.shape)
+
+    while curr_tol >= tol:
+        grad = dev(x0)
+        dir = -1*grad
+
+        prev_x = x0
+        x0 = x0 + lr*dir
+
+        curr_tol = np.linalg.norm(x0 - prev_x)
+
+        max_iters -= 1
+
+        if verbose:
+            print("Iterations rem:", max_iters, ", x =", x0, ", tol =", curr_tol)
+
+        if max_iters <= 0:
+            break
+
+    return x0
